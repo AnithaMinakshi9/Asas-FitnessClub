bafcb67295e3e1ef316ba48d6d7b364fb126a890
Anitha Minakshi <62071531+AnithaMinakshi9@users.noreply.github.com>
2020-12-06 12:08:38 +0530

    Update SliderData.js

--- Client/src/pages/SliderData.js
+++ Client/src/pages/SliderData.js
@@ -1,7 +1,7 @@
 export const SliderData = [
   {
     image:
-      "https://www.hotelieracademy.org/wp-content/uploads/2019/10/banyan-tree-fitness-club-masthead-d.jpg"
+      "https://media.istockphoto.com/photos/happy-athletic-people-jogging-on-treadmills-in-a-health-club-picture-id1089939832?k=6&m=1089939832&s=612x612&w=0&h=o3mjGEu7ru4R-_seQ6PFJ5b90qTIy5c8K5XWsRW-4dk="
   },
   {
     image:
